6c2799d6742b69d56f4b1ef3e27af3e2c66a9a3d
marcomastrolonardo <marco24091999@gmail.com>
2023-06-29 15:24:04 +0200

    modifiche ad articles.index

--- progetto_finale/resources/views/livewire/article-index-list.blade.php
+++ progetto_finale/resources/views/livewire/article-index-list.blade.php
@@ -21,29 +21,31 @@
  
 
   <div class="row">
-    <div class="col-lg-4 col-sm-6 mb-4">
-      @foreach ($articles as $article)
-      <div class="card h-100">
-        <a href="#"><img class="card-img-top" src="https://via.placeholder.com/700x400" alt=""></a>
-        <div class="card-body">
-          <h4 class="card-title">
-            <h3>{{$article->title}}</h3>
-            <hr>
-          </h4>
-          <p class="card-text">Prezzo: {{$article->price}}</p>
-          
-          <p class="card-text">Descrizione: {{$article->description}}</p>
-          
-          <p class="card-text">Categoria: {{$article->category->name}}</p>
-          <a href="{{route('articles.show', $article)}}" class="btn btn-sm btn-outline-info">Dettagli</a>
-          <a href="{{route('articles.edit', $article)}}" class="btn btn-sm btn-outline-warning">Modifica</a>
-          <livewire:article-delete-form :article="$article"/>
+    @foreach ($articles as $article)
+      <div class="col-lg-4 col-sm-6 mb-4">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="https://via.placeholder.com/700x400" alt=""></a>
+          <div class="card-body">
+            <h4 class="card-title">
+              <h3>{{$article->title}}</h3>
+              <hr>
+            </h4>
+            <p class="card-text">Prezzo: {{$article->price}}</p>
+            
+            <p class="card-text">Descrizione: {{$article->description}}</p>
+            
+            <p class="card-text">Categoria: {{$article->category->name}}</p>
+            <a href="{{route('articles.show', $article)}}" class="btn btn-sm btn-outline-info">Dettagli</a>
+            <a href="{{route('articles.edit', $article)}}" class="btn btn-sm btn-outline-warning">Modifica</a>
+            <livewire:article-delete-form :article="$article"/>
+          </div>
         </div>
       </div>
-      @endforeach
-    </div>
+    @endforeach
+    <!-- Paginazione -->
     {{$articles->links()}}
   </div>
+
   <!-- Pagination -->
   <ul class="pagination justify-content-center">
     <li class="page-item">
