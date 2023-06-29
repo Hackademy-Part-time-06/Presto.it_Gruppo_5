94b0cc2103750e26412ec1991bb2c748be950148
eleonora princigalli <ele.princigalli@gmail.com>
2023-06-29 16:06:42 +0200

    aggiustate le card

--- progetto_finale/resources/views/components/homecard.blade.php
+++ progetto_finale/resources/views/components/homecard.blade.php
@@ -1,26 +1,14 @@
-
-                <!-- Portfolio item 1-->
-
-                <div class="portfolio-item">
-                    <a class="portfolio-link" data-bs-toggle="modal">
-                        <div class="portfolio-hover">
-                            <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
-                        </div>
-                        <img class="img-fluid" src="/media/lavandino.jpeg" alt="..." />
-                    </a>
-                    <div class="portfolio-caption">
-                        <div class="portfolio-caption-heading">{{ $latestarticle->title }}</div>
-                        <div class="portfolio-caption-heading">{{ $latestarticle->price }}</div>
-                        <div class="portfolio-caption-subheading text-muted">{{ $latestarticle->category->name }}</div>
-                        <a href="{{ route('articles.show', $latestarticle) }}" class="btn btn-outline-secondary">See
-                            details</a>
-
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-sm-6 mb-4">
-
-            </div>
-        </div>
+<div class="card h-100">
+    <a href="#"><img class="card-img-top" src="https://via.placeholder.com/700x400" alt=""></a>
+    <div class="card-body">
+        <h4 class="card-title">
+            <h3>{{ $latestarticle->title }}</h3>
+            <hr>
+        </h4>
+        <p class="card-text">Prezzo: {{ $latestarticle->price }}</p>
+        <p class="card-text">Categoria: {{ $latestarticle->category->name }}</p>
+        <a href="{{ route('articles.show', $latestarticle) }}" class="btn btn-outline-secondary">See
+            details</a>
     </div>
-</section>
+</div>
+
--- progetto_finale/resources/views/homepage.blade.php
+++ progetto_finale/resources/views/homepage.blade.php
@@ -10,13 +10,16 @@
             <h2 class="section-heading text-uppercase">I nostri ultimi articoli</h2>
             <h3 class="section-subheading text-muted">Non perderti le nostre offerte.</h3>
         </div>
+        
         <div class="row">
-            
             @foreach ($latestarticles as $latestarticle)
+           
             <div class="col-lg-4 col-sm-6 mb-4">
                 <x-homecard :latestarticle="$latestarticle" />
             </div>
+            
             @endforeach
+        </div>
         
     </div>
 </x-main>
