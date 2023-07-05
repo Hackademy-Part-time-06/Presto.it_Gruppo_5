b04944a13cd2b9a87b5a6fe97239fef19cc5309a
eleonora princigalli <ele.princigalli@gmail.com>
2023-07-05 22:32:24 +0200

    aggiustata sezione categorie

--- progetto_finale/resources/views/livewire/category-list.blade.php
+++ progetto_finale/resources/views/livewire/category-list.blade.php
@@ -4,22 +4,23 @@
             <h2 class="section-heading">Esplora le nostre categorie</h2>
         </div>
         <div class="text-center">
-            @foreach ($categories as $category)
-                <div class="container-fluid containerCat">
-                    <div class="row mx-5">
-                        <div class="col-md-3 wrapper ">
-                            <div class="cardCat"> <i class="icona bi bi-bag"></i>
+            <div class="container-fluid containerCat">
+                <div class="row mx-5">
+
+                    @foreach ($categories as $category)
+                        <div class="col-md-3 wrapper">
+                            <div class="cardCat mb-5"> <i class="icona bi bi-bag"></i>
 
                                 <h5><a class="scritta"
                                         href="{{ route('category.show', $category) }}">{{ $category->name }}</a>
                                 </h5>
                             </div>
                         </div>
-                    </div>
-                </div>
 
 
-                {{-- <div class="col-md-3 my-3">
+
+
+                        {{-- <div class="col-md-3 my-3">
                     <div class="mx-5">
                         <div class="circle mx-2 ">
                             <h1> <i class="icona bi bi-bag"></i></h1>
@@ -29,10 +30,13 @@
                         <h5><a class="scritta" href="{{ route('category.show', $category) }}">{{ $category->name }}</a>
                         </h5>
                     </div>
-                </div> --}}
-            @endforeach
+                    </div> --}}
+                    @endforeach
+                </div>
+            </div>
         </div>
     </div>
-    </div>
+    
+    
 
 </section>
