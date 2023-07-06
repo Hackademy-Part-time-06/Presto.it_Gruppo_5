5b84b746a47df35335ef2d61ca38d519ab70a750
eleonora princigalli <ele.princigalli@gmail.com>
2023-07-06 21:14:54 +0200

    card mail

--- progetto_finale/resources/views/mail/submit.blade.php
+++ progetto_finale/resources/views/mail/submit.blade.php
@@ -1,13 +1,50 @@
 <x-main>
+  {{--   <div class='container'>
+        <div class="row">
+            <div class="col-md-12">
+            <div class="body-submit">
+    <div class="card-submit">
+        <div class="content text-center">
+            <h1>Vuoi unirti al nostro team?</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et 
+            dolore magna aliqua</p>
+            <button class="btn btn-outline-danger" type="submit"><a href="{{ route('become.revisor') }}">clicca qui per lavorare con noi</a></button>
+        </div>
+    </div>
+</div>
+</div>
+</div> --}}
 
-    
-     
-    
-    
+     <div class='container'>
+        <div class="row">
+            <div class="col-md-12">
+            <div class="body-submit">
+                <div class="card-submit">
+                    <div class="card-info-submit text-center">
+                        <p>Vuoi entrare a fare parte del nostro team?
+                            
+                           <a class="card-info-submit" href="{{ route('become.revisor') }}">clicca qui per lavorare con noi</a>
+
+                        
+                        </p>
+                        
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+ 
 
 
 
-{{-- <div class='cointaner'>
+
+
+
+
+
+    {{-- <div class='container-fluid'>
 
     <li><a href="{{ route('become.revisor') }}">clicca qui per lavorare con noi</a></li>
 </div> --}}
