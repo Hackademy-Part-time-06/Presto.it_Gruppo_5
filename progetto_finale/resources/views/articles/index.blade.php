edbcaea1d982dc96b38b07edca53be452637e82c
marcomastrolonardo <marco24091999@gmail.com>
2023-07-06 20:07:52 +0200

    Implementazione della ricerca per categorie

--- progetto_finale/resources/views/articles/index.blade.php
+++ progetto_finale/resources/views/articles/index.blade.php
@@ -1,10 +1,9 @@
 <x-main>
 
     <div class="container">
-        <form action="{{route('articles.search')}}" method="GET" class="my-5">
-            <input type="search" name="searched" class="form-control my-2" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+        
+        <x-search-bar />
+        
 
         <x-article-index-list :articles="$articles" />
     </div>
