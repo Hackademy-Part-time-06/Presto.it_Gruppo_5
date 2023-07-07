3eb6998a865009b7612e3ed12a4297ff67a1e29b
eleonora princigalli <ele.princigalli@gmail.com>
2023-07-07 20:41:02 +0200

    modifiche grafiche

--- progetto_finale/resources/views/components/navbar.blade.php
+++ progetto_finale/resources/views/components/navbar.blade.php
@@ -3,7 +3,7 @@
     <ul>
         <li><a class="nav-link nav-button ps-2 @if (Route::currentRouteName() == 'home') active @endif" aria-current="page"
                 href="{{ route('home') }}">Home</a></li>
-        <li><a class='dropdown-arrow' href='#'>Articoli</a>
+        <li><a class='dropdown-arrow' href='#'>Articoli <i class="bi bi-caret-down"></i> </a>
             <ul class='sub-menus'>
                 <li><a href="{{ route('articles.index') }}">Articoli inseriti</a></li>
                 <li><a href="{{ route('articles.create') }}">Inserisci un articolo</a></li>
@@ -11,7 +11,7 @@
 
             </ul>
         </li>
-        <li><a class='dropdown-arrow' href='#'>Categorie</a>
+        <li><a class='dropdown-arrow' href='#'>Categorie<i class="bi bi-caret-down"></i> </a>
             <ul class='sub-menus'>
 
                 @foreach ($categories as $category)
@@ -51,7 +51,7 @@
             <div class="justify-content-end px-3">
                 <li>
 
-                    <a class="dropdown-arrow ms-2 " href="#">Benvenut* @auth {{ Auth::user()->name }}
+                    <a class="dropdown-arrow ms-2 " href="#" > Benvenut* @auth {{ Auth::user()->name }} <i class="bi bi-caret-down"></i> 
                         @else
                         Guest @endauth
                     </a>
@@ -74,7 +74,7 @@
 
                     </ul>
 
-                <li><a class='dropdown-arrow' href='#'><i class="bi bi-globe"></i></a>
+                <li><a class='dropdown-arrow' href='#'><i class="bi bi-globe"></i><i class="ms-2 bi bi-caret-down"></i> </a>
                     <ul class='sub-menus'>
                         <li><a href="#">IT</a></li>
                         <li><a href="#">EN</a></li>
