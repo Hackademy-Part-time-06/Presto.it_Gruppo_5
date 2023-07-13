fd449b63b16ae78a530411fd90d4a8f46423a40a
eleonora princigalli <ele.princigalli@gmail.com>
2023-07-13 18:54:49 +0200

    user story 6

--- progetto_finale/app/Models/Image.php
+++ progetto_finale/app/Models/Image.php
@@ -5,16 +5,36 @@ namespace App\Models;
 use App\Models\Article;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Storage;
 
 class Image extends Model
 {
     use HasFactory;
 
-    protected $fillable=['path'];
+    protected $fillable = ['path'];
 
-    public function article(){
+    public function article()
+    {
         return $this->belongsTo(Article::class); //relazione 1 to N le immagini appartengono ad un articolo
     }
 
     //creo le funzioni per recuperare le img croppate
+
+    public static function getUrlbyFilePath($filePath, $w = null, $h = null)
+    {
+        //se abbiamo chiamato questa funzione senza passare altezza e larghezza dell'img restituiamo l'img originale
+        if (!$w && !$h)
+            return Storage::url($filePath);
+            //altrimenti andremo a recuperare l'img croppata 
+            $path= dirname($filePath);
+            $fileName= basename($filePath);
+            $file= "{$path}/crop_{$w}x{$h}_{$fileName}";
+            return Storage::url($file);
+
+    }
+
+    public function getUrl($w = null, $h = null)
+    {
+        return Image::getUrlbyFilePath($this->path, $w, $h);
+    }
 }
