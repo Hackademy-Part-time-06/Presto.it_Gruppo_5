db34fea96ce2a62dba5b1797dd34ea8392300ec1
eleonora princigalli <ele.princigalli@gmail.com>
2023-07-13 17:21:38 +0200

    modificato il path per img crop

--- progetto_finale/app/Http/Livewire/ArticleCreateForm.php
+++ progetto_finale/app/Http/Livewire/ArticleCreateForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Jobs\ResizeImage;
 use Livewire\Component;
 use App\Models\Article;
 use App\Models\Category;
@@ -22,45 +23,48 @@ class ArticleCreateForm extends Component
         'category_id'        => 'required',
         'title'              => 'required|min:4',
         'price'              => 'required|numeric',
-        'description'        => 'required|min:8',
+        'description'        => 'required',
         'images.*'           => 'image|max:1024',
         'temporary_images.*' => 'image|max:1024',
     ];
 
     protected $messages = [
-        'required'=>'Il campo :attribute è richiesto',
-        'min'     =>'Il campo :attribute è troppo corto',
-        'numeric' =>'Il campo :attribute deve essere di tipo numerico',
-        'temporary_images.required'=> "L'immagine è richiesta",
-        'temporary_images.*.image'=> "Il file deve essere un' immagine",
-        'temporary_images.*.max'=> "L'immagine deve essere di 1 mb",
-        'images.image'=> "Il file deve essere un' immagine",
-        'images.max'=> "L'immagine deve essere di 1 mb",
+        'required' => 'Il campo :attribute è richiesto',
+        'min'     => 'Il campo :attribute è troppo corto',
+        'numeric' => 'Il campo :attribute deve essere di tipo numerico',
+        'temporary_images.required' => "L'immagine è richiesta",
+        'temporary_images.*.image' => "Il file deve essere un' immagine",
+        'temporary_images.*.max' => "L'immagine deve essere di 1 mb",
+        'images.image' => "Il file deve essere un' immagine",
+        'images.max' => "L'immagine deve essere di 1 mb",
     ];
 
-    public function updatedTemporaryImages(){
-        if($this->validate([
+    public function updatedTemporaryImages()
+    {
+        if ($this->validate([
             'temporary_images.*' => 'image|max:1024',
-        ])){
-            foreach($this->temporary_images as $image){
+        ])) {
+            foreach ($this->temporary_images as $image) {
                 $this->images[] = $image;
             }
         }
     }
-    
+
     //Permette di rimuovere l'immagine dall'array images in base alla chiave dell'immagine passata 
-    public function removeImage($key){
-        if(in_array($key, array_keys($this->images))){
+    public function removeImage($key)
+    {
+        if (in_array($key, array_keys($this->images))) {
             unset($this->images[$key]);
         }
     }
-    
-    public function store(){
+
+    public function store()
+    {
 
         $this->validate();
-        
+
         $category = Category::find($this->category_id);
-        
+
         $this->article = $category->articles()->create([
             //Ottengo l'id dell'user
             'user_id'       => Auth::user()->id,
@@ -68,11 +72,17 @@ class ArticleCreateForm extends Component
             'price'         => $this->price,
             'description'   => $this->description,
         ]);
-        if(count($this->images)){
-            foreach($this->images as $image){
-                $this->article->images()->create(
-                    ['path'=>$image->store('images', 'public')]
+        if (count($this->images)) {
+            foreach ($this->images as $image) {
+                /* $this->article->images()->create(
+                    ['path' => $image->store('images', 'public')] 
+                ); */
+
+                $newFileName = "articles/{$this->article->id}"; //creamo un cartella articles con all'interno l'id dell'annuncio
+                $newImage = $this->article->images()->create(           //ogni annuncio avrà l'img croppata in questa cartella
+                    ['path' => $image->store($newFileName, 'public')]
                 );
+                dispatch(new ResizeImage($newImage->path , 700 , 400)); //al job appena creato passo il path dell'img salva e le dimensioni che voglio dell'img 
             }
         }
         /*
@@ -84,20 +94,21 @@ class ArticleCreateForm extends Component
             'description'   => $this->description,
         ]);
         */
-        
 
-        $this->reset('user_id','category_id','title','price','description','images','temporary_images');
-        
+
+        $this->reset('user_id', 'category_id', 'title', 'price', 'description', 'images', 'temporary_images');
+
         session()->flash('article', 'Articolo inserito correttamente');
         /*
         return redirect()
                 ->route('articles.index')
                     ->with('success','Articolo inserito correttamente');
-        */          
+        */
     }
 
     //Validazione real-time
-    public function updated($propertyName){
+    public function updated($propertyName)
+    {
         $this->validateOnly($propertyName);
     }
 
--- progetto_finale/app/Jobs/ResizeImage.php
+++ progetto_finale/app/Jobs/ResizeImage.php
@@ -2,25 +2,27 @@
 
 namespace App\Jobs;
 
+use Spatie\Image\Image;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Spatie\Image\Manipulations;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 
 class ResizeImage implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-   private $fileName; //nome dell'img
-   private $path; //path dove salvare l'img
-   private $w;
-   private $h;
+    private $fileName; //nome dell'img
+    private $path; //path dove salvare l'img
+    private $w;
+    private $h;
 
 
-     //inserisco come parametri del costruttore il path,nome, altezza e larghezza dell'img 
-    public function __construct($filePath,$w,$h)
+    //inserisco come parametri del costruttore il path,nome, altezza e larghezza dell'img 
+    public function __construct($filePath, $w, $h)
     {
         $this->path->dirname($filePath); //inserisco il percorso in questa variabile
         $this->fileName->basename($filePath); //inserisco il nome in questa variabile
@@ -33,9 +35,12 @@ class ResizeImage implements ShouldQueue
      */
     public function handle(): void
     {
-        $w= $this->w;
-        $h= $this->h;
-        $scrPath= storage_path() . 'app/public/' . $this->path . '/' . $this->fileName; //variabile che contiene il path da cui prendere l'img
-        $destPath= storage_path() .'app/public/' . $this->path . "/crop_{$w}x{$h}_" . $this->fileName;//variabile che contiene il path dove salvare l'img croppata
+        $w = $this->w;
+        $h = $this->h;
+        $srcPath = storage_path() . 'app/public/' . $this->path . '/' . $this->fileName; //variabile che contiene il path da cui prendere l'img
+        $destPath = storage_path() . 'app/public/' . $this->path . "/crop_{$w}x{$h}_" . $this->fileName; //variabile che contiene il path dove salvare l'img croppata
+        $croppedImage = Image::load($srcPath)
+            ->crop(Manipulations::CROP_CENTER, $h, $w) //->crop(Manipulations::CROP_TOP_RIGHT, 250, 250)
+            ->save($destPath);
     }
 }
