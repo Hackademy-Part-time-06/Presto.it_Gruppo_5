b7101997a5f716a9b9a6a02771ef206baf8e1392
Serena_Brunetti <serena.brunetti@libero.it>
2023-07-13 21:28:12 +0200

    Migrazione

--- progetto_finale/resources/views/templates/main.blade.php
+++ progetto_finale/resources/views/templates/main.blade.php
@@ -6,11 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Presto.it</title>
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 </head>
 
-<body id="vanta" >
+<body id="vanta">
 
     <x-navbar />
 
