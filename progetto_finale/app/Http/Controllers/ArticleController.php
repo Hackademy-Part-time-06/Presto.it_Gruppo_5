b5fa0b38b9aff8efd5d2517f18ed555be6769018
Serena_Brunetti <serena.brunetti@libero.it>
2023-07-17 17:47:41 +0200

    Lang categorie

--- progetto_finale/app/Http/Controllers/ArticleController.php
+++ progetto_finale/app/Http/Controllers/ArticleController.php
@@ -14,7 +14,7 @@ class ArticleController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth')->except('index', 'show', 'search');
+        $this->middleware('auth')->except('index', 'show', 'search', 'setLanguage');
     }
 
     //Search
