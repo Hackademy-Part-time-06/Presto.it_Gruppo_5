66ed3a192c3d9ea818ca316d46ef24795fd3b93d
Serena_Brunetti <serena.brunetti@libero.it>
2023-07-20 20:24:44 +0200

    Footer

--- progetto_finale/resources/views/components/footer.blade.php
+++ progetto_finale/resources/views/components/footer.blade.php
@@ -12,12 +12,12 @@
     </footer>
 </div> --}}
 
-<div class="footer d-none d-lg-block">
+<div class="footer d-none d-lg-block fixed-bottom">
 
     <footer class=" bg-dark footer py-2">
-        <div class="container">
-            <div class="row align-items-center justify-content-between">
-                <div class="col-lg-4 text-lg-start" style="color: rgb(212, 212, 212)">Copyright &copy; Your Website 2023
+        <div class="container-fluid justify-content-between">
+            <div class="row align-items-center mx-5">
+                <div class="col-lg-4 text-lg-start" style="color: rgb(212, 212, 212)">Copyright &copy; Presto.it 2023
                 </div>
                 <div class="col-lg-4 my-lg-0 text-lg-center">
                     <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Twitter"><i
@@ -30,8 +30,11 @@
                             class="bi bi-instagram"></i></i></a>
                 </div>
                 <div class="col-lg-4 text-lg-end">
-                    <a class=" text-decoration-none me-3" style="color: brown" href="#!">Privacy Policy</a>
-                    <a class="text-decoration-none" style="color: brown" href="#!">Terms of Use</a>
+                    <div class="" style="color: rgb(212, 212, 212)">Created by
+                        Eleonora Princigalli, Serena Brunetti, Marco Mastrolonardo
+                    </div>
+
+
                 </div>
             </div>
         </div>
